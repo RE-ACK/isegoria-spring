57c092b107ae4e6133666f2629773f14efcee704
gyulo94 <qkrcjfrb112@gmail.com>
2026-05-09 00:43:42 +0900

    first commit

---
+++ src/test/java/com/isegoria/server/ServerApplicationTests.java
@@ -0,0 +1,12 @@
+package com.isegoria.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
